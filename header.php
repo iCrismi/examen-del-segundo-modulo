804e4e65689f88b6b0354f5a054765ad81ad2a5f
iCrismi <icrismff27@outlook.com>
2025-10-28 21:56:11 -0500

    inicio - examen del segundo modulo

---
+++ header.php
@@ -0,0 +1,27 @@
+<header class="text-bg-dark p-3">
+  <div class="container">
+    <div class="">
+      <a href="#" class="text-decoration-none"><i class="bi bi-bootstrap"> Bootstrap</i></a>
+      <ul class="nav col-12 col-lg-auto mb-2 justify-content-center">
+        <li><a href="#" class="nav-link text-white px-5">Inicio</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Categorias</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Ofertas / Promociones</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Ropa y accesorios</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Ectrónica y tecnología</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Hogar y decoración</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Jugues / Niños</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Carrito de Compras</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Opiniones / Reseñas</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Contacto / Ayuda</a></li>
+        <li><a href="#" class="nav-link text-white px-5">Alumnos</a></li>
+      </ul>
+      <form class="col-12 col-lg-auto mb-3 mb-lg-0 mb-md-0" role="search">
+        <input type="search" class="" placeholder="Buscar items" aria-label="Search">
+      </form>
+      <div class="text-end">
+        <button type="button" class="btn btn-outline-light me-2">Login</button>
+        <button type="button" class="btn btn-warning">Sign-up</button>
+      </div>
+    </div>
+  </div>
+</header>
