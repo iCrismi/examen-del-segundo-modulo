64a7a6ad6f3bdd14bebb7552c5ae3ac403737996
iCrismi <icrismff27@outlook.com>
2025-10-30 07:55:03 -0500

    commit "Avance pero es algo"

--- footer.php
+++ footer.php
@@ -2,4 +2,13 @@
   <p>© 2025 Mi Tienda Virtual | FIISI "Ingenieria Informatica"</p>
 </footer>
 </body>
+
 </html>
+
+<footer class="text-center mt-5 p-3 container">
+  <p class="float-end"><a href="#">Back to top</a></p>
+  <p>
+    &copy; 2017–2025 Company, Inc. &middot;
+    <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
+  </p>
+</footer>
